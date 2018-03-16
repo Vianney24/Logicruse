6ddf86f3daa0068b14e479ad95f04dec1d5d99a2
Yaourt404 <Rems_40@hotmail.fr>
2018-03-16 16:39:31 +0100

    test

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
                         </a>
                     </li>
                 </ul>
-                <p class="copyright text-muted">Copyright &copy; Your Website 2017</p>
+                <p class="fa-copyright">Copyright &copy; Logicruse 2018</p>
             </div>
         </div>
     </div>
