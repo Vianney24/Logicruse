e7aaf130cda9b72044cab79e2be4cc0961147de1
Vianney CHEPEAU <vianney.chepeau@gmail.com>
2018-03-16 17:15:23 +0100

    modification header

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
             Menu
             <i class="fa fa-bars"></i>
         </button>
@@ -9,21 +11,22 @@
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">Accueil</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="informations.php">Informations</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="activite.php">Activités</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="inscription.php">Inscription</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="connexion.php">Connexion</a>
-                </li>
+                <?php if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="monCompte.php">Mon Compte</a>
+                    </li>
+                    <?php
+                } else {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="inscription.php">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="connexion.php">Connexion</a>
+                    </li>
+                    <?php
+                }
+                ?>
             </ul>
         </div>
     </div>
